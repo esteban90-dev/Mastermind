b0436fda0d4aded471908fc3f089b4c160c243cd
Stephen Allard <stephencallard@gmail.com>
2020-02-14 10:58:11 -0600

    Added display method to the Board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,41 @@
-class Board
-    attr_reader :grid
+require './guess_cell.rb'
+require './feedback_cell.rb'
 
-    def initialize
-        @grid = default_grid
+class Board
+    attr_reader :guesses, :feedbacks, :rounds
+
+    def initialize(rounds=8)
+        @guesses = empty_guess_array(rounds)
+        @feedbacks = empty_fdbk_array(rounds)
+        @rounds = rounds
     end
 
     public
 
     def display
-        grid.each{ |x| puts x.join }
+        i = 0
+        while i < rounds
+            puts "-------------------"
+            puts "\t\t" + feedbacks[0].slice(0,feedbacks[0].length/2).map{ |x| x.get_cell }.join
+            puts "\t\t" + feedbacks[0].slice(feedbacks[0].length/2, feedbacks[0].length-1).map{ |x| x.get_cell }.join
+            puts guesses[i].map{ |x| x.get_cell }.join(" ")
+            i += 1
+        end
     end
 
     private 
 
-    def default_grid
-        Array.new(10){ Array.new(8){ |x| x < 4 ? 1:0 }}
+    def empty_guess_array(rounds)
+        Array.new(rounds){ Array.new(4) { GuessCell.new }}
+    end
+
+    def empty_fdbk_array(rounds)
+        Array.new(rounds){ Array.new(4) { FeedbackCell.new }}
     end
 end
 
-a = Board.new
+a = Board.new(16)
 a.display
+
+
 
