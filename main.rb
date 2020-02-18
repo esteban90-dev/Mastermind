c40338702fdc2731cd9eee46dcc629d91d9d8616
Stephen Allard <stephencallard@gmail.com>
2020-02-17 19:43:09 -0600

    Added game replay functionality

--- main.rb
+++ main.rb
@@ -5,10 +5,24 @@ require './lib/code_maker.rb'
 require './lib/code_breaker.rb'
 require './lib/peg.rb'
 
-board_1 = Board.new(4)
-code_maker_1 = CodeMaker.new
-code_breaker_1 = CodeBreaker.new
+def replay?
+    input = 0
+    puts "Would you like to play again? Enter Y/N."
+    loop do
+        input = gets.chomp.upcase
+        break if "YN".include?(input)
+        puts "Please enter valid input."
+    end
+    input == "Y"
+end
 
-game_1 = Game.new(board_1,code_breaker_1,code_maker_1)
-game_1.play
-game_1.display_results
+loop do
+    board_1 = Board.new(1)
+    code_maker_1 = CodeMaker.new
+    code_breaker_1 = CodeBreaker.new
+
+    game_1 = Game.new(board_1,code_breaker_1,code_maker_1)
+    game_1.play
+    game_1.display_results
+    break if !replay?
+end
